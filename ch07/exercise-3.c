f2b6e0f4dd0710d512474a71b7cb1d3cd8a9ad24
Jeong-dap <njd990603@naver.com>
2026-05-22 15:27:32 +0900

    semapore and sharedMemory

---
+++ ch07/exercise-3.c
@@ -0,0 +1,52 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/msg.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+int main()
+{
+	int shmid, i,j ;
+	char *shmaddr;
+
+	if((shmid=shmget(0x123400, 30, 0660|IPC_CREAT|IPC_EXCL))==-1) {
+		if((shmid=shmget(0x123400, 30, 0660))==-1){
+			perror("shmget");
+			exit(1);
+		}
+	}
+
+    union semnum {
+        int val;
+        struct semid_ds *buf;
+        unsigned short *arr;
+        struct seminfo* sembuf;
+    } semctrl;
+
+    semctrl.val = 0;
+    semctl(shmid, 0, SETVAL, semctrl);
+    
+
+ 	if((shmaddr=shmat(shmid, (char *)0, 0))== NULL) {
+		perror("shmat");
+		exit(1);
+	}
+ 	for(i=0; i<20; i++) {
+ 		sprintf(shmaddr, "shared memory test %d", i+1);
+ 		printf("send : %s\n", shmaddr);
+ 		for(j=0; j<100000000; j++);
+ 	}
+ 	sprintf(shmaddr, "end");
+
+	if(shmdt(shmaddr)==-1 ) {
+		perror("shmdt");
+		exit(1);
+	}
+
+	return 0;
+}
